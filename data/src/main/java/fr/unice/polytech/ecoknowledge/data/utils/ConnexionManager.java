7382c02e2a8690ceb90debf1023d205ddec08ac8
Benjamin Benni <benjamin.benni06@gmail.com>
2015-11-30 22:43:21 +0100

    Add URI to MongoClient constructor
    I need to sleep heroku.

--- data/src/main/java/fr/unice/polytech/ecoknowledge/data/utils/ConnexionManager.java
+++ data/src/main/java/fr/unice/polytech/ecoknowledge/data/utils/ConnexionManager.java
@@ -1,6 +1,7 @@
 package fr.unice.polytech.ecoknowledge.data.utils;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
@@ -20,7 +21,9 @@ public class ConnexionManager {
     private ConnexionManager() {
         System.out.println("BDD URL is set to " + System.getenv("MONGOHQ_URL"));
 
-        mongo = new MongoClient(System.getenv("MONGOHQ_URL"));
+
+
+        mongo = new MongoClient(new MongoClientURI(System.getenv("MONGOHQ_URL")));
     }
 
     public static ConnexionManager getInstance() {
