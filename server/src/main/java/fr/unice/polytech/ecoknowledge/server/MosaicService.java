f67d46c558656b0932d3e530149bcb4197dfe8ff
Sebastien <petillon.sebastien@gmail.com>
2015-12-09 16:24:06 +0100

    Temporarily added the mosaic for 1.1 requirements

---
+++ server/src/main/java/fr/unice/polytech/ecoknowledge/server/MosaicService.java
@@ -0,0 +1,42 @@
+package fr.unice.polytech.ecoknowledge.server;
+
+import com.google.gson.JsonArray;
+import fr.unice.polytech.ecoknowledge.domain.Controller;
+import fr.unice.polytech.ecoknowledge.domain.data.GoalNotFoundException;
+import fr.unice.polytech.ecoknowledge.domain.data.exceptions.IncoherentDBContentException;
+import fr.unice.polytech.ecoknowledge.domain.data.exceptions.NotReadableElementException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
+/**
+ * Created by Sébastien on 09/12/2015.
+ *
+ *
+ * Just now to keep the 1.1 Requirement for the 13/12/15 delivery
+ *
+ */
+
+@Path("/mosaic")
+public class MosaicService {
+
+    @GET
+    public Response getUserMosaic(@QueryParam("userID") String userID){
+        System.out.println("MOSAIC");
+        JsonArray resultJsonArray = new JsonArray();
+        try {
+            resultJsonArray.addAll(Controller.getInstance().getNotTakenChallengesOfUser(userID));
+            resultJsonArray.addAll(Controller.getInstance().getTakenChallengesOfUser(userID));
+            resultJsonArray.addAll(Controller.getInstance().getGoalsResultOfUser(userID));
+        } catch (IncoherentDBContentException | GoalNotFoundException | IOException | NotReadableElementException e) {
+            e.printStackTrace();
+            return Response.status(500).build();
+        }
+
+        return Response.ok().entity(resultJsonArray.toString()).build();
+    }
+
+}
