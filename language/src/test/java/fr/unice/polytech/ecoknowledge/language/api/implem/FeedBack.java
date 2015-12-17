c29d7b7fa0d3923ff898b2506dbd3f3084e9288b
Benjamin Benni <benjamin.benni06@gmail.com>
2015-12-17 18:33:31 +0100

    Add feedback file

---
+++ language/src/test/java/fr/unice/polytech/ecoknowledge/language/api/implem/FeedBack.java
@@ -0,0 +1,36 @@
+package fr.unice.polytech.ecoknowledge.language.api.implem;
+
+import fr.unice.polytech.ecoknowledge.language.api.implem.enums.OLD_PERIOD;
+import org.junit.Test;
+
+import static fr.unice.polytech.ecoknowledge.language.api.implem.enums.DURATION_TYPE.WEEK;
+
+/**
+ * Created by Sébastien on 30/11/2015.
+ */
+public class FeedBack {
+
+	@Test
+	public void tryToCreateAChallengeOnYourOwn() {
+
+		//	L'idée est de vous faire expérimenter notre DSL et avoir vos retours (:
+		//	A la fin du fichier vous avez 3 questions plutôt ouvertes mais qui devraient être rapides !
+		//	Créer le challenge que vous voulez ! Si vous voulez en faire plusieurs, AVEC PLAISIR !
+		//	Renvoyez moi le fichier une fois fini (héhé)
+
+		//	Merci é-no-mément !
+
+		//	It all starts with Challenge.create ...
+
+	}
+}
+
+/*
+
+Est-ce que la description du défi a été rapide ? Facile ?
+
+Est-ce que vous auriez voulu exprimer des informations différemment ? (modification de mots-clés ou de syntaxe)
+
+Voudriez-vous rajouter des manières de décrire d'autres types de défis ? (“pisser la porte fermée“, etc.)
+
+*/
