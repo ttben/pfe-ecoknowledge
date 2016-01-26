e4d071208638ecbfd97c25c999e86ccac70ff3b0
Benjamin Benni <benjamin.benni06@gmail.com>
2016-01-26 13:16:08 +0100

    Update WebTierFeeder
    Add new HTTP ROUTE : DELETE / will drop content of TRACKING_REQUESTS
    collection
    Add check nto POST /, in order to do nothing when the target sensor is
    already tracked

--- feeder/webTierFeeder/src/main/java/WebTierFeeder.java
+++ feeder/webTierFeeder/src/main/java/WebTierFeeder.java
@@ -1,13 +1,13 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.List;
 
 @Path("/")
 public class WebTierFeeder {
@@ -17,13 +17,32 @@ public class WebTierFeeder {
 	@POST
 	@Consumes("application/json")
 	@Produces("application/json")
-	public Response addNewTrackingRequest(String trackingRequestDescription) {
+	public Response addNewTrackingRequest(String trackingRequestDescription) throws IOException {
 		JsonObject trackingRequestJsonDescription = new JsonParser().parse(trackingRequestDescription).getAsJsonObject();
+
+		TrackRequestPOJO newTrackingRequest = new ObjectMapper().readValue(trackingRequestDescription.toString(), TrackRequestPOJO.class);
+		List<TrackRequestPOJO> trackRequestPOJOList = DBFeederLayer.getAllTrackingRequests();
+
+		for(TrackRequestPOJO currentTrackingRequest : trackRequestPOJOList) {
+
+			//	Check if sensor is already tracked
+			if(currentTrackingRequest.hasSameTargetThan(newTrackingRequest)) {
+				return Response.ok().build();
+			}
+		}
+
+
 
 		// System.out.println(trackingRequestJsonDescription);
 
 		bdd.storeTrackingRequest(trackingRequestJsonDescription);
 
+		return Response.ok().build();
+	}
+
+	@DELETE
+	public Response deleteAllTrackingRequests() {
+		bdd.deleteAllTrackingRequests();
 		return Response.ok().build();
 	}
 } 
