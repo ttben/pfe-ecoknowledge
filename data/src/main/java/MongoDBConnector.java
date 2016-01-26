79d9d9beee645e51179f0c80b7bf3112c4a1dae9
Benjamin Benni <benjamin.benni06@gmail.com>
2016-01-26 13:11:00 +0100

    Add service into monngoDBConnector
    Add deleteAllTrackingRequests method into mongoDBConnector

--- data/src/main/java/MongoDBConnector.java
+++ data/src/main/java/MongoDBConnector.java
@@ -300,4 +300,7 @@ public class MongoDBConnector implements DocumentBDDConnector {
 		logger.info(dbName + " dropped !");
 	}
 
+	public void deleteAllTrackingRequests() {
+		getCollection(TRACKING_REQUESTS_COLLECTION).drop();
+	}
 }
