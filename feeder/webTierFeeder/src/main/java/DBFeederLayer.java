dac721acfb562778a5e680c2643fca31f326a0b6
Benjamin Benni <benjamin.benni06@gmail.com>
2016-01-26 13:14:47 +0100

    Add 'DBFeederLayer' to wrap use of Jackson

---
+++ feeder/webTierFeeder/src/main/java/DBFeederLayer.java
@@ -0,0 +1,30 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Benjamin on 26/01/2016.
+ */
+public class DBFeederLayer {
+	private static MongoDBConnector dbConnector = MongoDBConnector.getInstance();
+
+	public static List<TrackRequestPOJO> getAllTrackingRequests() throws IOException {
+		List<TrackRequestPOJO> result = new ArrayList<>();
+
+		JsonArray trackingRequestsJsonArray = dbConnector.findAllTrackingRequest();
+
+		for(JsonElement currentTrackingRequestElement : trackingRequestsJsonArray) {
+			JsonObject currentTrackingRequestJsonObject = currentTrackingRequestElement.getAsJsonObject();
+			ObjectMapper mapper = new ObjectMapper();
+			TrackRequestPOJO currentTrackingRequest = mapper.readValue(currentTrackingRequestJsonObject.toString(), TrackRequestPOJO.class);
+			result.add(currentTrackingRequest);
+		}
+
+		return result;
+	}
+}
