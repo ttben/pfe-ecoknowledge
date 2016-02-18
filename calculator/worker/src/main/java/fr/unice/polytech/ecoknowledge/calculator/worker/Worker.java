204ef5b2125bd31bb8dbc92c2a715918163d281e
Benjamin Benni <benjamin.benni06@gmail.com>
2016-02-18 12:15:17 +0100

    #138 transfert Goal instead of string into worker

--- calculator/worker/src/main/java/fr/unice/polytech/ecoknowledge/calculator/worker/Worker.java
+++ calculator/worker/src/main/java/fr/unice/polytech/ecoknowledge/calculator/worker/Worker.java
@@ -1,8 +1,17 @@
 package fr.unice.polytech.ecoknowledge.calculator.worker;
 
+import fr.unice.polytech.ecoknowledge.data.exceptions.GoalNotFoundException;
+import fr.unice.polytech.ecoknowledge.data.exceptions.NotReadableElementException;
+import fr.unice.polytech.ecoknowledge.data.exceptions.NotSavableElementException;
+import fr.unice.polytech.ecoknowledge.data.exceptions.UserNotFoundException;
+import fr.unice.polytech.ecoknowledge.domain.calculator.Calculator;
+import fr.unice.polytech.ecoknowledge.domain.model.Goal;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.jms.*;
+import java.io.IOException;
 
 /**
  * Created by Benjamin on 21/01/2016.
@@ -11,6 +20,9 @@ public class Worker implements Runnable, ExceptionListener, MessageListener {
 	static int number = 0;
 	private String name;
 	private int fakeProcessingTime;
+
+	final Logger logger = LogManager.getLogger(Worker.class);
+
 
 	public Worker(String name, int fakeProcessingTime) {
 		this.name = name;
@@ -25,20 +37,45 @@ public class Worker implements Runnable, ExceptionListener, MessageListener {
 	public void onMessage(Message message) {
 
 		try {
-			if (message instanceof TextMessage) {
-				TextMessage textMessage = (TextMessage) message;
-				String text = textMessage.getText();
-				System.out.println(name + " received: " + text + ". Now sleeping for " + fakeProcessingTime + " ms");
+			if (message instanceof ObjectMessage) {
+				ObjectMessage objectMessage = (ObjectMessage) message;
+				Object obj = objectMessage.getObject();
 
+				if(!(obj instanceof Goal)) {
+					String errorDescription = "A calculator worker just received a non Goal object: ";
+					errorDescription = errorDescription.concat(obj.getClass().getName());
+
+					logger.fatal(errorDescription);
+
+					throw new IllegalArgumentException(errorDescription);
+				}
+
+				Goal goal = (Goal) obj;
+
+				System.out.println(name + " received: " + goal.getId() + ". Now sleeping for " + fakeProcessingTime + " ms");
+				Calculator calculator = new Calculator(null);
+				calculator.evaluate(goal);
 
 				Thread.sleep(fakeProcessingTime);
 
 			} else {
 				System.out.println("OTHER Received: " + message);
 			}
-		} catch (JMSException e) {
+		}
+		// TODO: 18/02/2016 LOG AND HANDLE IT
+		catch (JMSException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (GoalNotFoundException e) {
+			e.printStackTrace();
+		} catch (UserNotFoundException e) {
+			e.printStackTrace();
+		} catch (NotSavableElementException e) {
+			e.printStackTrace();
+		} catch (NotReadableElementException e) {
 			e.printStackTrace();
 		}
 	}
