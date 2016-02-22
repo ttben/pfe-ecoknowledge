a1a0a4a4424c6e41e523d2819f00f0d501c2e9e7
Sebastien <petillon.sebastien@gmail.com>
2016-02-22 23:17:29 +0100

    ActiveMQ Working - test of ActiveMQ not working

--- integration/src/script/integration.bat
+++ integration/src/script/integration.bat
@@ -6,12 +6,14 @@ SET datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:
 SET LOG_FILE=logs\%datetimef%.log
 SET LOG_FILE_TOMCAT_START=logs\%datetimef%_tomcat_start.log
 SET LOG_FILE_TOMCAT_STOP=logs\%datetimef%_tomcat_stop.log
+SET LOG_FILE_ACTIVEMQ=logs\%datetimef%_activeMQ.log
 SET SOURCE_MODULE_RELATIVE_PATH=..\..\..\fakeDataSource
 SET SOURCE_NAME=fakeDataSource
 SET SOURCE_WAR_NAME=fakeDataSource.war
 SET TOMCAT_HOST=http://localhost:8080/
 SET TOMCAT_SERVICE=fakeDataSource/
-SET TIMEOUT=20
+SET ACTIVEMQ_HOST=http://localhost:8161/
+SET TIMEOUT=30
 
 SET "INTEGRATION_HOME=%CD%"
 ECHO INTEGRATION_HOME : %INTEGRATION_HOME% > %INTEGRATION_HOME%\%LOG_FILE%
@@ -81,5 +83,47 @@ IF NOT ["%tomcatResponse%"] == ["{"testOk":true}"] (
 ECHO Service Avalaible
 ECHO Service Avalaible >> %INTEGRATION_HOME%\%LOG_FILE%
 
+IF ["%ACTIVEMQ_HOME%"] == [""] (
+	ECHO ERROR : ACTIVEMQ_HOME is not defined >> %INTEGRATION_HOME%\%LOG_FILE%
+	ECHO ERROR : ACTIVEMQ_HOME is not defined
+	GOTO :end
+)
+
+ECHO Starting ActiveMQ ...
+ECHO /---------- Starting ActiveMQ -----------/ >> %INTEGRATION_HOME%\%LOG_FILE_ACTIVEMQ%
+START "" "%ACTIVEMQ_HOME%\bin\activemq.bat" start  >> %INTEGRATION_HOME%\%LOG_FILE_ACTIVEMQ% 2>>&1
+ECHO /--------------------------------------/ >> %INTEGRATION_HOME%\%LOG_FILE_ACTIVEMQ%
+ECHO >> %INTEGRATION_HOME%\%LOG_FILE_ACTIVEMQ%
+
+
+:: Wait for activeMQ to be ready
+ping 127.0.0.1 -n %TIMEOUT% > nul
+
+
+ECHO Calling %ACTIVEMQ_HOST% >> %INTEGRATION_HOME%\%LOG_FILE%
+CALL "%CURL_HOME%\curl.exe" -get %ACTIVEMQ_HOST% >> %INTEGRATION_HOME%\%LOG_FILE%
+CALL "%CURL_HOME%\curl.exe" -get %ACTIVEMQ_HOST% > %INTEGRATION_HOME%\activeMQResponse
+
+setlocal ENABLEDELAYEDEXPANSION
+set vidx=0
+for /F "tokens=*" %%A in (%INTEGRATION_HOME%\activeMQResponse) do (
+    SET /A vidx=!vidx! + 1 > nul
+    set var!vidx!=%%A > nul
+)
+set var > nul
+
+IF NOT ["%var1%"] == [""] (
+    ECHO COUCOU
+)
+
+::IF ["%activeMQResponse%"] == [""] (
+::	ECHO ERROR : ActiveMQ can't be reached >> %INTEGRATION_HOME%\%LOG_FILE%
+::	ECHO ERROR : ActiveMQ can't be reached
+::	GOTO :end
+::)
+DEL "%INTEGRATION_HOME%\activeMQResponse"
+
 :end
 CD %INTEGRATION_HOME%
+
+:: CLOSE EVERYTHING
