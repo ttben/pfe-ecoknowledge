3a07835b792c0065759ff7eaf312b7950c670fca
Sebastien <petillon.sebastien@gmail.com>
2016-02-24 16:14:24 +0100

    Test without building mail.jar

--- integration/src/script/packageMailJar.sh
+++ integration/src/script/packageMailJar.sh
@@ -5,7 +5,7 @@ echo // ----- Maven package : mail >> "$INTEGRATION_HOME/$LOG_FILE"
 
 cd ../..
 
-mvn package >> /dev/null 2>> "$INTEGRATION_HOME/$LOG_FILE"
+#mvn package >> /dev/null 2>> "$INTEGRATION_HOME/$LOG_FILE"
 
 rm $INTEGRATION_HOME/mail.jar >> /dev/null 2>> "$INTEGRATION_HOME/$LOG_FILE"
 cp target/integration-1.0-SNAPSHOT-jar-with-dependencies.jar $INTEGRATION_HOME/mail.jar >> /dev/null 2>> "$INTEGRATION_HOME/$LOG_FILE"
