0b73ab9a1c1f46fe301f6088db43aea4c2a8358d
Sebastien <petillon.sebastien@gmail.com>
2016-02-24 18:08:49 +0100

    Try to deploy ecoknowledge

--- integration/src/script/cpyWar.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo
-echo // ----- Copying war
-echo // ----- Copying war >> "$INTEGRATION_HOME/$LOG_FILE"
-
-rm "$CATALINA_HOME/webapps/$SOURCE_WAR_NAME" >> /dev/null 2>> "$INTEGRATION_HOME/$LOG_FILE"
-rm -rf "$CATALINA_HOME/webapps/$SOURCE_NAME" >> /dev/null 2>> "$INTEGRATION_HOME/$LOG_FILE"
-cp "target/$SOURCE_WAR_NAME" "$CATALINA_HOME/webapps/$SOURCE_WAR_NAME" >> /dev/null 2>> "$INTEGRATION_HOME/$LOG_FILE"
--- integration/src/script/mvnBuild.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo
-echo // ----- Maven build
-echo // ----- Maven build >> "$INTEGRATION_HOME/$LOG_FILE"
-
-mvn package >> /dev/null 2>> "$INTEGRATION_HOME/$LOG_FILE"
