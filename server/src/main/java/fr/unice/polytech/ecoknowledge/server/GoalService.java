2a27a09752a585c9222f10960b3332f43aea8e6a
Sebastien <petillon.sebastien@gmail.com>
2016-02-26 00:17:16 +0100

    Allow cross over goal service post (take challenge)

--- server/src/main/java/fr/unice/polytech/ecoknowledge/server/GoalService.java
+++ server/src/main/java/fr/unice/polytech/ecoknowledge/server/GoalService.java
@@ -30,7 +30,12 @@ public class GoalService {
 
 		try {
 			JsonObject result = Controller.getInstance().takeChallenge(jsonObject);
-			return Response.ok().entity(result.toString()).build();
+			return Response.ok().entity(result.toString())
+					.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+					.header("Access-Control-Allow-Credentials", "true")
+					.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+					.header("Access-Control-Max-Age", "1209600")
+					.build();
 		} catch (JsonMappingException | JsonParseException e) {
 			e.printStackTrace();
 			System.out.println(e.getMessage());
--- server/src/main/java/fr/unice/polytech/ecoknowledge/server/UserService.java
+++ server/src/main/java/fr/unice/polytech/ecoknowledge/server/UserService.java
@@ -29,20 +29,10 @@ public class UserService {
 					.build();
 		} catch (IOException e) {
 			e.printStackTrace();
-			return Response.status(403).entity(e.getStackTrace())
-					.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                    .header("Access-Control-Allow-Credentials", "true")
-                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                    .header("Access-Control-Max-Age", "1209600")
-					.build();
+			return Response.status(403).entity(e.getStackTrace()).build();
 		} catch (NotSavableElementException e) {
 			e.printStackTrace();
-			return Response.status(500).entity(e.getMessage())
-                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                    .header("Access-Control-Allow-Credentials", "true")
-                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                    .header("Access-Control-Max-Age", "1209600")
-					.build();
+			return Response.status(500).entity(e.getMessage()).build();
 		}
 	}
 
