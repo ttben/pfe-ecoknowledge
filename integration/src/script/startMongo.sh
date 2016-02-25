198ac14c2c399f183e1e8dbb9493f574f669aa06
Benjamin Benni <benjamin.benni06@gmail.com>
2016-02-25 16:17:53 +0100

    Create and delete TMP DB files location

--- integration/src/script/startMongo.sh
+++ integration/src/script/startMongo.sh
@@ -3,4 +3,6 @@ echo
 echo // ----- Launching mongoDB
 echo // ----- Launching mongoDB >> "$LOG_FILE"
 
+mkdir -p "$DB_PATH"
+
 mongod --dbpath "$DB_PATH" & >> /dev/null 2>> "$INTEGRATION_HOME/$LOG_FILE"
